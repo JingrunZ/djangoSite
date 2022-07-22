f9b6aa37a2b643f9cedc243cf267429f6f015683
JingrunZ <stanfordz753@gmail.com>
2022-07-22 16:21:39 -0400

    web scrapping

--- myapp/1.py
+++ myapp/1.py
@@ -1,20 +1,77 @@
 import requests
 from bs4 import BeautifulSoup
-from models import wealth
+import re
+import pandas as pd
+import csv
+import glob
 
-wealth_list= "http://quotes.money.163.com/trade/lsjysj_000001.html?year=2022&season=2"
+
+#2610,2636
+
+
+
+
+#rows = tbody.find_all('tr', recursive=False)
+
+
+    #print(row.find_all('td')[0])
+
+#result = res.find_all('table',{"class": "table_bg001 border_box limit_sale"})
+#for i in result:
+#    print(i)
+    
+#x = re.findall(".\d{7}[.]html", str(res))
+#tempName = res.find('a',href=x[0]).text
+#for i in result.find_all('td')[0].text:
+#print(res.find_all('td')[0].text)
+        
+
+# tempDate = result[i].find_all('td')[0].text
+# tempOpen =  result[i].find_all('td')[1].text
+# tempMax = result[i].find_all('td')[2].text
+# tempMin = result[i].find_all('td')[3].text
+# tempClose = result[i].find_all('td')[4].text
+# tempUpDown = result[i].find_all('td')[5].text
+# tempQuote = result[i].find_all('td')[6].text
+# tempVol = result[i].find_all('td')[7].text
+# tempTurnover = result[i].find_all('td')[8].text
+# tempAmplitude = result[i].find_all('td')[9].text
+# tempTurnoverRate = result[i].find_all('td')[10].text
+
+#df = pd.read_csv(r'C:\Users\stanf\OneDrive\Desktop\project\mysite\myapp\tushare.csv')
+#for i in range(2643,4735):
+#    wealth_list= "http://quotes.money.163.com/trade/lsjysj_600519.html?year=2022&season=2"
+#    will_replace = df.loc[i][0][0:6]
+    
+
+#    wealth_list = re.sub("[0-9][0-9][0-9][0-9][0-9][0-9]", will_replace , wealth_list)
+#    print(wealth_list)
+    
+    
+wealth_list = "http://quotes.money.163.com/trade/lsjysj_600519.html?year=2022&season=2"
 temp=requests.get(wealth_list)
+print(wealth_list)
 temp.encoding=temp.apparent_encoding
 new=temp.text
 res = BeautifulSoup(new,'html.parser')
 
 result = res.find_all('tr')
-tempName = res.find('a',href="/1000001.html").text
-
-for i in range(5,64):
-    tempDate = result[i].find_all('td')[0].text
-
-    for i in wealth.objects.all():
-        if i.name != tempName and i.date != tempDate:
-            print("ok")
     
+x = re.findall(".\d{7}[.]html", str(res))
+tempName = res.find('a',href=x[0]).text
+
+for row in res.find_all('table',{"class":'table_bg001 border_box limit_sale'})[0].find_all('tr')[1:]:
+    tempDate = row.find_all('td')[0].text
+    tempOpen =  row.find_all('td')[1].text
+    tempMax = row.find_all('td')[2].text
+    tempMin = row.find_all('td')[3].text
+    tempClose = row.find_all('td')[4].text
+    tempUpDown = row.find_all('td')[5].text
+    tempQuote = row.find_all('td')[6].text
+    tempVol = row.find_all('td')[7].text
+    tempTurnover = row.find_all('td')[8].text
+    tempAmplitude = row.find_all('td')[9].text
+    tempTurnoverRate = row.find_all('td')[10].text
+    print(tempOpen)
+        
+        
--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,9 @@
+from MySQLdb import IntegrityError
 from django.db import models
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import re
 
 # Create your models here.
 class info(models.Model):
@@ -26,86 +29,86 @@ class wealth(models.Model):
     Turnover = models.CharField('成交金额', max_length=100)
     amplitude = models.CharField('振幅', max_length=100)
     TurnoverRate = models.CharField('换手率', max_length=100)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'date'],name='unique_booking')
+        ]
 
     def __str__(self):
         return self.name
-
-url1 = 'http://quotes.money.163.com/f10/gszl_000002.html#01f01'
-text=requests.get(url1)
-text.encoding=text.apparent_encoding
-text=text.text
-bs4 = BeautifulSoup(text,'html.parser')
-#get company name
-CompanyName = bs4.find('a',href="/1000002.html").text
-
-rows=bs4.find_all('td')
-#find opening price
-i=0
-for price in rows:
-    if price.text == "上市首日开盘价":
-        openIndex = i
-    i += 1
-openPrice = rows[openIndex+1].text
-#find closing price
-j=0
-for price in rows:
-    if price.text == "上市首日收盘价":
-        closeIndex = j
-    j += 1
-closePrice = rows[closeIndex+1].text
-
-#find address
-a=0
-for address in rows:
-    if address.text == "办公地址":
-        addressIndex = a
-    a+=1
-companyAddress = rows[addressIndex+1].text
-
-#find establishment date
-b=0
-for est in rows:
-    if est.text == "成立日期":
-        estIndex = b
-    b+=1
-estdate = rows[estIndex+1].text
-
-c=0
-for marDate in rows:
-    if marDate.text == "上市日期":
-        marketIndex = c
-    c+=1
-marketDate = rows[marketIndex+1].text
-
-
+#----------------------------------------------------------------------------
+url1 = 'http://quotes.money.163.com/f10/gszl_000001.html#01f01'
+posts = []
 for i in info.objects.all():
-    if i.name != CompanyName:
+    posts.append(i.name)
+
+
+
+df = pd.read_csv(r'C:\Users\stanf\OneDrive\Desktop\project\mysite\myapp\tushare.csv')
+for i in range(2643,len(df)):
+    url1 = re.sub("[0-9][0-9][0-9][0-9][0-9][0-9]", df.loc[i][0][0:6] , url1)
+    text=requests.get(url1)
+    print(url1)
+    text.encoding=text.apparent_encoding
+    text=text.text
+    bs4 = BeautifulSoup(text,'html.parser')
+
+    x = re.findall(".\d{7}[.]html", str(bs4))
+    CompanyName = bs4.find('a',href=x[0]).text
+
+    rows = bs4.find_all('tr')
+
+    companyAddress=rows[5].find_all('td')[3].text
+    openPrice=rows[28].find_all('td')[1].text
+    closePrice=rows[29].find_all('td')[1].text
+    marketDate=rows[17].find_all('td')[1].text
+    estdate=rows[16].find_all('td')[1].text
+    
+    
+
+    if CompanyName not in posts:
         b = info(name=CompanyName, openingPrice=openPrice, closingPrice=closePrice, address=companyAddress,establishment=estdate,marketTime=marketDate)
         b.save()
+        posts.append(CompanyName)
 
-#------------------------------------------------------------------
 
-wealth_list= "http://quotes.money.163.com/trade/lsjysj_000001.html?year=2022&season=2"
-temp=requests.get(wealth_list)
-temp.encoding=temp.apparent_encoding
-new=temp.text
-res = BeautifulSoup(new,'html.parser')
+# #------------------------------------------------------------------
+# 
+df = pd.read_csv(r'C:\Users\stanf\OneDrive\Desktop\project\mysite\myapp\tushare.csv')
+for i in range(4684,4735):
+    wealth_list= "http://quotes.money.163.com/trade/lsjysj_000001.html?year=2022&season=2"
+    will_replace = df.loc[i][0][0:6]
+    
 
-result = res.find_all('tr')
-tempName = res.find('a',href="/1000001.html").text
-for i in range(5,64):
-    tempDate = result[i].find_all('td')[0].text
-    tempOpen =  result[i].find_all('td')[1].text
-    tempMax = result[i].find_all('td')[2].text
-    tempMin = result[i].find_all('td')[3].text
-    tempClose = result[i].find_all('td')[4].text
-    tempUpDown = result[i].find_all('td')[5].text
-    tempQuote = result[i].find_all('td')[6].text
-    tempVol = result[i].find_all('td')[7].text
-    tempTurnover = result[i].find_all('td')[8].text
-    tempAmplitude = result[i].find_all('td')[9].text
-    tempTurnoverRate = result[i].find_all('td')[10].text
-    for i in wealth.objects.all():
-        if i.name != tempName and i.date != tempDate:
-            b = wealth(name=tempName,date=tempDate,open=tempOpen,max=tempMax,min=tempMax,upDown=tempUpDown,Quote=tempQuote,volume=tempVol,Turnover=tempTurnover,amplitude=tempAmplitude,TurnoverRate=tempTurnoverRate)
+    wealth_list = re.sub("[0-9][0-9][0-9][0-9][0-9][0-9]", will_replace , wealth_list)
+    temp=requests.get(wealth_list)
+    print(wealth_list)
+    temp.encoding=temp.apparent_encoding
+    new=temp.text
+    res = BeautifulSoup(new,'html.parser')
+
+    result = res.find_all('tr')
+    
+    x = re.findall(".\d{7}[.]html", str(res))
+    tempName = res.find('a',href=x[0]).text
+
+    for row in res.find_all('table',{"class":'table_bg001 border_box limit_sale'})[0].find_all('tr')[1:]:
+        tempDate = row.find_all('td')[0].text
+        tempOpen =  row.find_all('td')[1].text
+        tempMax = row.find_all('td')[2].text
+        tempMin = row.find_all('td')[3].text
+        tempClose = row.find_all('td')[4].text
+        tempUpDown = row.find_all('td')[5].text
+        tempQuote = row.find_all('td')[6].text
+        tempVol = row.find_all('td')[7].text
+        tempTurnover = row.find_all('td')[8].text
+        tempAmplitude = row.find_all('td')[9].text
+        tempTurnoverRate = row.find_all('td')[10].text
+        
+        if not wealth.objects.filter(name=tempName,date=tempDate).exists():
+            b = wealth(name=tempName,date=tempDate,open=tempOpen,max=tempMax,min=tempMin,upDown=tempUpDown,Quote=tempQuote,volume=tempVol,Turnover=tempTurnover,amplitude=tempAmplitude,TurnoverRate=tempTurnoverRate)  
             b.save()
+
+
+            
+
